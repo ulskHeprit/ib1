f537d8e8acee3c3cc1d3ed7917560de9239266a5
ulskHeprit <xumuk495.x@ya.ru>
2018-12-18 17:39:58 +0400

    123

--- ib/Form1.Designer.cs
+++ ib/Form1.Designer.cs
@@ -28,13 +28,184 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.generation_button = new System.Windows.Forms.Button();
+            this.maskedTextBox1 = new System.Windows.Forms.MaskedTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.label2 = new System.Windows.Forms.Label();
+            this.save_button = new System.Windows.Forms.Button();
+            this.load_button = new System.Windows.Forms.Button();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.clear_button = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.test1_button = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 6F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(2, 2);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox1.Size = new System.Drawing.Size(794, 331);
+            this.textBox1.TabIndex = 0;
+            // 
+            // generation_button
+            // 
+            this.generation_button.Location = new System.Drawing.Point(228, 336);
+            this.generation_button.Name = "generation_button";
+            this.generation_button.Size = new System.Drawing.Size(109, 23);
+            this.generation_button.TabIndex = 1;
+            this.generation_button.Text = "Сгенерировать";
+            this.generation_button.UseVisualStyleBackColor = true;
+            this.generation_button.Click += new System.EventHandler(this.generation_button_Click);
+            // 
+            // maskedTextBox1
+            // 
+            this.maskedTextBox1.Location = new System.Drawing.Point(94, 338);
+            this.maskedTextBox1.Mask = "000000000000";
+            this.maskedTextBox1.Name = "maskedTextBox1";
+            this.maskedTextBox1.Size = new System.Drawing.Size(128, 20);
+            this.maskedTextBox1.TabIndex = 2;
+            this.maskedTextBox1.ValidatingType = typeof(int);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(-1, 341);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(89, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Размер в битах:";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(62, 220);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(660, 20);
+            this.progressBar1.TabIndex = 4;
+            this.progressBar1.Visible = false;
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.WorkerReportsProgress = true;
+            this.backgroundWorker1.WorkerSupportsCancellation = true;
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.bw_DoWork);
+            this.backgroundWorker1.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.bw_ProgressChanged);
+            this.backgroundWorker1.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.bw_RunWorkerCompleted);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(370, 204);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "label2";
+            this.label2.Visible = false;
+            // 
+            // save_button
+            // 
+            this.save_button.Location = new System.Drawing.Point(508, 415);
+            this.save_button.Name = "save_button";
+            this.save_button.Size = new System.Drawing.Size(127, 23);
+            this.save_button.TabIndex = 6;
+            this.save_button.Text = "Сохранить в файл";
+            this.save_button.UseVisualStyleBackColor = true;
+            this.save_button.Click += new System.EventHandler(this.save_button_Click);
+            // 
+            // load_button
+            // 
+            this.load_button.Location = new System.Drawing.Point(661, 415);
+            this.load_button.Name = "load_button";
+            this.load_button.Size = new System.Drawing.Size(127, 23);
+            this.load_button.TabIndex = 7;
+            this.load_button.Text = "Загрузить из файла";
+            this.load_button.UseVisualStyleBackColor = true;
+            this.load_button.Click += new System.EventHandler(this.load_button_Click);
+            // 
+            // saveFileDialog1
+            // 
+            this.saveFileDialog1.DefaultExt = "txt";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.DefaultExt = "txt";
+            // 
+            // clear_button
+            // 
+            this.clear_button.Location = new System.Drawing.Point(444, 336);
+            this.clear_button.Name = "clear_button";
+            this.clear_button.Size = new System.Drawing.Size(75, 23);
+            this.clear_button.TabIndex = 8;
+            this.clear_button.Text = "Очистить";
+            this.clear_button.UseVisualStyleBackColor = true;
+            this.clear_button.Click += new System.EventHandler(this.clear_button_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(352, 336);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Отмена";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // test1_button
+            // 
+            this.test1_button.Location = new System.Drawing.Point(12, 415);
+            this.test1_button.Name = "test1_button";
+            this.test1_button.Size = new System.Drawing.Size(99, 23);
+            this.test1_button.TabIndex = 10;
+            this.test1_button.Text = "Частотный тест";
+            this.test1_button.UseVisualStyleBackColor = true;
+            this.test1_button.Click += new System.EventHandler(this.test1_button_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.test1_button);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.clear_button);
+            this.Controls.Add(this.load_button);
+            this.Controls.Add(this.save_button);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.maskedTextBox1);
+            this.Controls.Add(this.generation_button);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Лабораторная №1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button generation_button;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        public System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button save_button;
+        private System.Windows.Forms.Button load_button;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button clear_button;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button test1_button;
     }
 }
 
--- ib/Form1.cs
+++ ib/Form1.cs
@@ -7,14 +7,158 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Threading;
 
 namespace ib
 {
     public partial class Form1 : Form
     {
+        Random rand;
+        string str;
+        string filename;
+        int test1;
+
         public Form1()
         {
             InitializeComponent();
+            rand = new Random();
+        }
+
+        private void bw_DoWork(object sender, DoWorkEventArgs e)
+        {
+            for (int i = 1; i <= Convert.ToInt32(maskedTextBox1.Text); ++i)
+            {
+                if (backgroundWorker1.CancellationPending)//обработка отмены функции
+                {
+                    e.Cancel = true;
+                    return;
+                }
+                str += rand.Next(0, 2);//рандом 0 или 1
+                if (i % 10000 == 0)
+                { 
+                    backgroundWorker1.ReportProgress(i);//отправка прогресса в progresschanched
+                }
+            }
+
+            e.Result = str; // будет передано в RunWorkerComрleted
+        }
+
+        public void asd()
+        {
+            
+        }
+
+        private void bw_RunWorkerCompleted(object sender,
+    RunWorkerCompletedEventArgs e)
+        {
+            if (e.Cancelled)
+                MessageBox.Show("Работа генератора была отменена пользователем!");
+            else if (e.Error != null)
+                MessageBox.Show("Exception: " + e.Error);
+            else
+            {
+                MessageBox.Show("Генерация выполнена успешно.");
+                str = "";
+                textBox1.Text = e.Result.ToString();//выводим результат в текстбокс и разблокируем элементы формы
+                label2.Visible = false;
+                progressBar1.Visible = false;
+                generation_button.Enabled = true;
+                maskedTextBox1.Enabled = true;
+                save_button.Enabled = true;
+                load_button.Enabled = true;
+                clear_button.Enabled = true;
+                test1_button.Enabled = true;
+            }
+        }
+
+        private void bw_ProgressChanged(object sender,
+          ProgressChangedEventArgs e)
+        {            
+            progressBar1.Value = e.ProgressPercentage*100/Convert.ToInt32(maskedTextBox1.Text);//вывод прогресса в процентах
+            label2.Text = progressBar1.Value+"%";
+        }
+
+
+        private void generation_button_Click(object sender, EventArgs e)
+        {
+            if (maskedTextBox1.Text!="")
+            {
+                str = "";
+                backgroundWorker1.RunWorkerAsync();//запускаем функцию и блокируем элементы формы
+                label2.Visible = true;
+                progressBar1.Visible = true;
+                generation_button.Enabled = false;
+                maskedTextBox1.Enabled = false;
+                save_button.Enabled = false;
+                load_button.Enabled = false;
+                clear_button.Enabled = false;
+                test1_button.Enabled = false;
+            }
+        }
+
+        private void save_button_Click(object sender, EventArgs e)
+        {
+            if (saveFileDialog1.ShowDialog() == DialogResult.Cancel)//сохранение в файл
+                return;
+            filename = saveFileDialog1.FileName;
+            // сохраняем текст в файл
+            System.IO.File.WriteAllText(filename, textBox1.Text);
+            MessageBox.Show("Файл сохранен");
+        }
+
+        private void load_button_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.Cancel)//загрузка из файла
+                return;
+            // получаем выбранный файл
+            filename = openFileDialog1.FileName;
+            // читаем файл в строку
+            string fileText = System.IO.File.ReadAllText(filename);
+            textBox1.Text = fileText;
+            MessageBox.Show("Загружено.");
+        }
+
+        private void clear_button_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";//очистка текстбокса
+        }
+
+        private void button1_Click(object sender, EventArgs e)//кнопка отмена
+        {
+            backgroundWorker1.CancelAsync();//отмена функции
+            label2.Visible = false;
+            progressBar1.Visible = false;
+            generation_button.Enabled = true;
+            maskedTextBox1.Enabled = true;
+            save_button.Enabled = true;
+            load_button.Enabled = true;
+            clear_button.Enabled = true;
+            test1_button.Enabled = true;
+        }
+
+        private void test1_button_Click(object sender, EventArgs e)
+        {
+            if(textBox1.Text!="")//частотный тест
+            {
+                test1 = 0;
+                str = textBox1.Text;
+                foreach (char ch in str)
+                {
+                    if(ch=='0')
+                    {
+                        test1 -= 1;
+                    }
+                    else
+                    {
+                            test1 += 1;
+                    }
+                }
+                MessageBox.Show("Кол-во: "+str.Length+" Сумма: "+test1.ToString()+" Статистика (S): "+(Math.Abs(test1)/Math.Sqrt(str.Length)).ToString() + (Math.Abs(test1) / Math.Sqrt(str.Length)<= 1.82138636?" Последовательность случайна.":" Последовательность не случайна."));
+            }
+            else
+            {
+                MessageBox.Show("Нечего проверять.");
+            }
         }
     }
 }
